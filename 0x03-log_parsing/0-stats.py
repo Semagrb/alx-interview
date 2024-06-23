2b4cb6746ca85bc5e9c164bcc9c4d3e8491a8ee4
Salma Fithi <128839637+Semagrb@users.noreply.github.com>
2024-06-23 22:32:09 +0100

    Update 0-stats.py

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,58 +1,54 @@
 #!/usr/bin/python3
+"""
+log parsing
+"""
+
 import sys
-import signal
+import re
 
-# Initialize metrics
-total_file_size = 0
-status_code_count = {
-    200: 0,
-    301: 0,
-    400: 0,
-    401: 0,
-    403: 0,
-    404: 0,
-    405: 0,
-    500: 0
-}
+def output(log: dict) -> None:
+    """
+    Helper function to display stats
+    """
+    print("File size: {}".format(log["file_size"]))
+    for code in sorted(log["code_frequency"]):
+        if log["code_frequency"][code]:
+            print("{}: {}".format(code, log["code_frequency"][code]))
 
-line_count = 0
+if __name__ == "__main__":
+    regex = re.compile(
+        r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3} - \[\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}.\d+\] "GET /projects/260 HTTP/1.1" (.{3}) (\d+)'
+    )
 
-def print_stats():
-    global total_file_size, status_code_count
-    print(f"File size: {total_file_size}")
-    for code in sorted(status_code_count.keys()):
-        if status_code_count[code] > 0:
-            print(f"{code}: {status_code_count[code]}")
+    line_count = 0
+    log = {}
+    log["file_size"] = 0
+    log["code_frequency"] = {
+        str(code): 0 for code in [
+            200, 301, 400, 401, 403, 404, 405, 500
+        ]
+    }
 
-def signal_handler(sig, frame):
-    print_stats()
-    sys.exit(0)
-
-signal.signal(signal.SIGINT, signal_handler)
-
-for line in sys.stdin:
-    line_count += 1
-    parts = line.split()
-    
-    # Validate the format of the line
-    if len(parts) != 10:
-        continue
-    
-    ip, dash, date, request, status_code, file_size = parts[0], parts[1], parts[2], parts[3] + " " + parts[4] + " " + parts[5], parts[8], parts[9]
-    
     try:
-        status_code = int(status_code)
-        file_size = int(file_size)
-    except ValueError:
-        continue
+        for line in sys.stdin:
+            line = line.strip()
+            match = regex.fullmatch(line)
+            if match:
+                line_count += 1
+                code = match.group(1)
+                file_size = int(match.group(2))
 
-    if request == "\"GET /projects/260 HTTP/1.1\"":
-        total_file_size += file_size
-        if status_code in status_code_count:
-            status_code_count[status_code] += 1
-    
-    if line_count % 10 == 0:
-        print_stats()
+                # Update file size
+                log["file_size"] += file_size
 
-# Print final stats if the script finishes without interruption
-print_stats()
+                # Update status code frequency
+                if code.isdecimal() and code in log["code_frequency"]:
+                    log["code_frequency"][code] += 1
+
+                if line_count % 10 == 0:
+                    output(log)
+    except KeyboardInterrupt:
+        output(log)
+        raise
+    finally:
+        output(log)
